c4e6453fd9ec849cd59bf51daa64bb40af38805f
Dipanjan Kailthya <dipanjan.kailthya@gmail.com>
2025-02-27 15:48:51 +0100

    Add location (#32)
    * Add a location to the database
    
    * Remove Path.
    
    ---------
    
    Co-authored-by: Dipanjan Kailthya <d.kailthya@energetecheurope.com>

--- python/src/etl_example/write_products.py
+++ python/src/etl_example/write_products.py
@@ -48,7 +48,14 @@ def main():
 
 
 def update_table(table_df: DataFrame, database_name: str, table_name: str) -> None:
-    table_df.write.format("delta").mode("overwrite").saveAsTable(f"{database_name}.{table_name}")
+    # glue_client = boto3.client('glue')
+    # response = glue_client.get_table(DatabaseName=database_name, Name=table_name)
+    # external_location = response["Table"]['StorageDescriptor']['Location']
+    table_df \
+        .write \
+        .format("delta") \
+        .mode("overwrite") \
+        .saveAsTable(f"{database_name}.{table_name}")
 
 
 def get_products(spark_session: SparkSession) -> DataFrame:
