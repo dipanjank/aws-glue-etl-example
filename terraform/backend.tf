20092443492c4de228c089d4a577eecaeae14923
Dipanjan Kailthya <d.kailthya@energetecheurope.com>
2025-02-27 13:38:52 +0100

    Fix apply error.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "dk-etl-example-terraform-state-bucket"
     key     = "terraform.tfstate"
-    region  = local.aws_region
+    region  = "eu-west-1"
     encrypt = true
   }
 }
