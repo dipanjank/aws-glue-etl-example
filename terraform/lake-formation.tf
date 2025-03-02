06d7229a52b93711b08738bf2754224964eaf9ee
Dipanjan Kailthya <d.kailthya@energetecheurope.com>
2025-03-02 17:39:02 +0100

    Create database, tables and LF permissions for Glue Role

--- terraform/lake-formation.tf
+++ terraform/lake-formation.tf
@@ -7,3 +7,31 @@ resource "aws_lakeformation_data_lake_settings" "lakeformation_admin" {
 resource "aws_lakeformation_resource" "lakeformation_bucket" {
   arn = aws_s3_bucket.sales_bucket.arn
 }
+
+# Lake Formation permissions for the ETL job
+resource "aws_lakeformation_permissions" "glue_etl_permissions" {
+  principal   = aws_iam_role.glue_job_role.arn
+  permissions = ["SELECT", "INSERT", "ALTER", "DESCRIBE"]
+
+  table {
+    database_name = aws_glue_catalog_database.sales_db.name
+    table_name    = aws_glue_catalog_table.products.name
+  }
+}
+
+# Lake Formation permissions for the ETL job
+resource "aws_lakeformation_permissions" "glue_etl_permissions" {
+  for_each = [
+    aws_glue_catalog_table.products.name,
+    aws_glue_catalog_table.product_sales.name,
+    aws_glue_catalog_table.daily_sales_by_category.name,
+  ]
+
+  principal   = aws_iam_role.glue_job_role.arn
+  permissions = ["SELECT", "INSERT", "ALTER", "DELTE", "DESCRIBE"]
+
+  table {
+    database_name = aws_glue_catalog_database.sales_db.name
+    table_name    = each.key
+  }
+}
